dbfcee056bb51da58f90dde1659edcf40f820871
erfankashani <erfan.e.k.kashani@gmail.com>
2019-01-21 17:08:25 -0500

    Association Rule Learning Models

--- 5-Association Rule Learning/Apriori/apriori.py
+++ 5-Association Rule Learning/Apriori/apriori.py
@@ -15,5 +15,5 @@ for i in range(0, 7501):
 from apyori import apriori
 rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
-# Visualising the results
+# Visualising the results onthe chart 
 results = list(rules)
